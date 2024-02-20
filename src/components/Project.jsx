bdbd9b1ef98aaf7ff1a400b43873c76a66399f04
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-02-21 00:21:22 +0530

    feat : new project section layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -21,7 +21,7 @@ const projects = [
       "Ollivanders Store is a Harry Potter-themed E-Commerce Wand store where you can purchase wands of your choice. The application ensures the secure handling and storage of user details by employing JWT-based authentication and PostgreSQL for the backend.",
     imageSrc: Ollivanders,
     altText: "Ollivanders Store",
-    link:"https://github.com/lokeshdarla/Ollivanders_store"
+    link: "https://github.com/lokeshdarla/Ollivanders_store"
   },
   {
     title: "Chemsite",
@@ -29,48 +29,80 @@ const projects = [
       "Chemsite is a responsive website designed for a chemistry professor to promote and showcase his research work while encouraging students towards the field of chemistry. Built using React, this platform serves as a dynamic space for highlighting the professor's research endeavors and fostering an interest in chemistry among students.",
     imageSrc: chemsite,
     altText: "Chemsite",
-    link:"https://github.com/lokeshdarla/chemsite",
-    previewLink:"https://chemsite.vercel.app/"
+    link: "https://github.com/lokeshdarla/chemsite",
+    previewLink: "https://chemsite.vercel.app/"
   },
   {
     title: "Unknown",
     description:
-    "Unknown is a pseudonymous share website where users' data will be encrypted with passlib, implemented using React.js, FastAPI, and Google Auth.",
+      "Unknown is a pseudonymous share website where users' data will be encrypted with passlib, implemented using React.js, FastAPI, and Google Auth.",
     imageSrc: Unknown,
     altText: "Unknown",
-    link:"https://github.com/lokeshdarla/Unknown"
+    link: "https://github.com/lokeshdarla/Unknown"
   },
 ];
 
-const ProjectCard = ({ title, description, imageSrc, altText,link,previewLink }) => (
-  <div className="flex flex-col-reverse items-center justify-center gap-8 p-5 sm:flex-row lg:flex-row lg:m-5">
-    <div className="flex flex-col w-full p-5 lg:m-5">
-      <h1 className="mb-4 text-2xl font-semibold text-gray-600 sm:text-3xl lg:text-3xl">{title}</h1>
-      <p className="content-around text-justify text-gray-500">{description}</p>
-      <div className="flex gap-2 my-5 sm:gap-5">
-        {skills.map((skill) => (
-          <a
-            href={skill.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            key={skill.id}
-            className="p-1 transition-shadow duration-300 ease-in-out bg-white border-gray-200 rounded-full shadow-sm hover:shadow-lg"
-          >
-            {skill.icon}
-          </a>
-        ))}
-      </div>
-      <div className='flex'>
-      <a href={link} className='px-5 py-2 rounded-full border text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none flex items-center gap-2 text-md'>Github<span><RxExternalLink/></span></a>
-      </div>
-      
+// const ProjectCard = ({ title, description, imageSrc, altText, link, previewLink }) => (
+//   <div className="flex flex-col-reverse items-center justify-center gap-8 p-5 sm:flex-row lg:flex-row lg:m-5">
+//     <div className="flex flex-col w-full p-5 lg:m-5">
+//       <h1 className="mb-4 text-2xl font-semibold text-gray-600 sm:text-3xl lg:text-3xl">{title}</h1>
+//       <p className="content-around text-justify text-gray-500">{description}</p>
+//       <div className="flex gap-2 my-5 sm:gap-5">
+//         {skills.map((skill) => (
+//           <a
+//             href={skill.link}
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             key={skill.id}
+//             className="p-1 transition-shadow duration-300 ease-in-out bg-white border-gray-200 rounded-full shadow-sm hover:shadow-lg"
+//           >
+//             {skill.icon}
+//           </a>
+//         ))}
+//       </div>
+//       <div className='flex'>
+//         <a href={link} className='px-5 py-2 rounded-full border text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none flex items-center gap-2 text-md'>Github<span><RxExternalLink /></span></a>
+//       </div>
+
+//     </div>
+//     <div className="lg:p-5">
+//       <figure className="">
+//         <img className="rounded-lg shadow-lg" src={imageSrc} alt={altText} />
+//         {previewLink && <a href={previewLink} target='_blank' className="flex items-center justify-center gap-2 mt-2 text-sm text-center text-gray-400 cursor-pointer hover:underline">Preview of the website<span><RxExternalLink /></span></a>}
+//       </figure>
+//     </div>
+//   </div>
+// );
+
+
+
+
+const ProjectCard = ({ title, description, imageSrc, altText, link, previewLink }) => (
+  <div className="flex items-center justify-center sm:flex-row lg:flex-row ">
+    <p href="#" class="group relative block h-68 w-96">
+  <span class="absolute inset-0 border-2 border-dashed border-black rounded-xl"></span>
+    
+  <div class="relative flex h-full transform items-center border-2 border-black rounded-xl bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+    
+    <div class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+    <img src={imageSrc} alt={altText} className='mt-2'/>
+  
+      <h2 class="mt-4 text-xl font-medium sm:text-2xl">{title}</h2>
+
     </div>
-    <div className="lg:p-5">
-      <figure className="">
-        <img className="rounded-lg shadow-lg" src={imageSrc} alt={altText} />
-       {previewLink && <a href={previewLink} target='_blank' className="flex items-center justify-center gap-2 mt-2 text-sm text-center text-gray-400 cursor-pointer hover:underline">Preview of the website<span><RxExternalLink /></span></a>} 
-      </figure>
+   
+    <div class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+      <h3 class="mt-4 text-xl font-medium sm:text-2xl">{title}</h3>
+
+      <p class="mt-4 text-sm sm:text-base">
+        {description}
+      </p>
+
+      <a href={link} className='px-5 py-2 mt-2  text-xs rounded-full border text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none items-center gap-2 text-md inline-flex'>Github<span><RxExternalLink /></span></a>
     </div>
+  </div>
+</p>
+
   </div>
 );
 
@@ -80,9 +112,12 @@ const ProjectSection = () => {
       <div className="max-w-2xl mx-auto text-center">
         <h1 className="mb-4 text-5xl font-bold text-gray-700">My Work</h1>
       </div>
+      <div className='flex flex-col items-center justify-center gap-8 lg:flex-row'> 
       {projects.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
+      </div>
+ 
     </div>
   );
 };
